ecf5e1038e332aefb00f0229fb9a4d571bd73eb0
waliori <waliori@gmail.com>
2026-02-23 23:13:33 -0500

    stable baseline before local data migration

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import QuranNetwork from '../quran_network_v8.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <QuranNetwork />
+  </StrictMode>,
+)
